cbcfaa44d8b4a9794f168d44c97948e1fe998339
Isabelle-C <isabellechen2023@u.northwestern.edu>
2024-03-11 16:54:23 -0500

    add some new code and linting

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,20 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-import MyMap from './MyMap';
+import { Link } from "react-router-dom";
+import MyMap from "./MyMap";
 import "leaflet/dist/leaflet.css";
 import "../css/map.css";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+import WorkIcon from "@mui/icons-material/Work";
+import SchoolIcon from "@mui/icons-material/School";
+import StarIcon from "@mui/icons-material/Star";
+
+import data from "../data.json"; // Path to your JSON file
+import education_data from "../data/timeline/education.json";
 
 const Home = () => (
   <div>
@@ -21,7 +33,7 @@ const Home = () => (
           <li>
             <Link to="/recipe">Recipe </Link>
           </li>
-          
+
           {/* Add more links as needed */}
         </ul>
       </nav>
@@ -29,6 +41,25 @@ const Home = () => (
     <h1>About Me</h1>
     <p> Personal page building still in progress...:)</p>
     {/* Add your bio here */}
+
+    <VerticalTimeline>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+        contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+        date={education_data.high_school.date}
+        iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+        icon={<SchoolIcon />}
+      >
+        <h3 className="vertical-timeline-element-title">
+          {education_data.high_school.title}
+        </h3>
+        <h4 className="vertical-timeline-element-subtitle">
+          {education_data.high_school.subtitle}
+        </h4>
+        <p>{education_data.high_school.date}</p>
+      </VerticalTimelineElement>
+    </VerticalTimeline>
 
     <div className="App">
       <MyMap />
